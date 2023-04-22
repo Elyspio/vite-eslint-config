902686f743eeae31d43212789b96d04049beaafe
Jonathan Guichard <jona.guich69@gmail.com>
2023-04-23 01:48:16 +0200

    Config -- Remove airbnb rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,120 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires,import/extensions
-const prettierConfig = require("./.prettierrc.js");
-
 module.exports = {
+	env: {
+		browser: true,
+		es6: true
+	},
+	"extends": [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended"
+	],
+	ignorePatterns: [
+		"**/generated/v*.ts",
+		"**/*.scss",
+		"**/*.png",
+		"**/*.svg"
+	],
 	parser: "@typescript-eslint/parser",
-	extends: ["airbnb", "airbnb-typescript", "airbnb/hooks", "prettier", "plugin:@typescript-eslint/recommended", "plugin:@typescript-eslint/recommended-requiring-type-checking"],
-	plugins: ["prettier"],
 	parserOptions: {
-		project: "./tsconfig.json",
+		ecmaFeatures: {
+			modules: true,
+			jsx: true
+		},
+		ecmaVersion: "latest",
+		sourceType: "module"
 	},
+	plugins: [
+		"prettier",
+		"import",
+		"@typescript-eslint",
+		"react",
+		"react-hooks"
+	],
 	rules: {
-		"@typescript-eslint/no-non-null-assertion": "off",
+		"@typescript-eslint/ban-ts-comment": "off",
+		"@typescript-eslint/explicit-function-return-type": "off",
+		"@typescript-eslint/explicit-module-boundary-types": "off",
+		"@typescript-eslint/no-empty-interface": "off",
 		"@typescript-eslint/no-explicit-any": "off",
-		"prettier/prettier": ["warn", prettierConfig],
+		"@typescript-eslint/no-non-null-assertion": "off",
+		"@typescript-eslint/no-shadow": [
+			"error",
+			{
+				allow: [
+					"_",
+				],
+			},
+		],
+		"@typescript-eslint/no-unused-vars": [
+			"error",
+			{
+				argsIgnorePattern: "^_",
+				caughtErrorsIgnorePattern: "^_",
+				varsIgnorePattern: "^_",
+			},
+		],
+		"@typescript-eslint/no-use-before-define": "off",
+		"import/extensions": "off",
+		"import/no-extraneous-dependencies": "off",
+		"import/order": "off",
 		"import/prefer-default-export": "off",
-		"no-void": "off"
+		"jsx-a11y/click-events-have-key-events": "off",
+		"jsx-a11y/control-has-associated-label": "off",
+		"jsx-a11y/interactive-supports-focus": "off",
+		"jsx-a11y/no-static-element-interactions": "off",
+		"linebreak-style": "off",
+		"max-classes-per-file": "off",
+		"max-len": [
+			"error",
+			{
+				code: 180,
+				ignoreRegExpLiterals: true,
+				ignoreStrings: true,
+				ignoreUrls: true,
+			},
+		],
+		"no-mixed-spaces-and-tabs": "off",
+		"no-shadow": "off",
+		"no-unused-vars": "off",
+		"prettier/prettier": [
+			"warn",
+			{
+				bracketSpacing: true,
+				endOfLine: "auto",
+				printWidth: 180,
+				singleQuote: false,
+				tabWidth: 4,
+				useTabs: true,
+			},
+		],
+		"react-hooks/exhaustive-deps": "error",
+		"react-hooks/rules-of-hooks": "error",
+		"react/jsx-filename-extension": [
+			"warn",
+			{
+				extensions: [
+					".tsx",
+				],
+			},
+		],
+		"react/jsx-props-no-spreading": "off",
+		"react/no-array-index-key": "warn",
+		"react/prop-types": "off",
 	},
-};
+	settings: {
+		"import/resolver": {
+			typescript: {
+				alwaysTryTypes: true
+			},
+			node: {
+				extensions: [
+					".ts",
+					".tsx"
+				]
+			}
+		},
+		react: {
+			version: "detect"
+		}
+	}
+}
+
