49b1dfd9e2bd93282f755fb3d5eaefc28462b167
Jonathan Guichard <jona.guich69@gmail.com>
2023-04-23 00:38:12 +0200

    Config -- Expose only vite config instead of vite instance

--- vite.config.js
+++ vite.config.js
@@ -1,50 +1,37 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-
-const { defineConfig } = require("vite");
 const { default: react } = require("@vitejs/plugin-react-swc");
 const { default: eslint } = require("vite-plugin-eslint");
-
-const path = require("path");
 const tsconfig = require("./tsconfig.json");
-
-/**
- * Convert typescripts "compilerOptions.paths" to vite/webpack alias
- * @param {Record<string, string[]>} paths
- * @param {string} basePath
- * @returns
- */
-function convertPathToAlias(paths, basePath) {
-	return Object.keys(paths)
-		.filter(p => p.endsWith("*"))
-		.reduce((acc, key) => {
-			const p = paths[key][0];
-			acc[key.slice(0, key.length - 2)] = path.resolve(basePath, p.slice(0, p.length - 1));
-			return acc;
-		}, {});
-}
+const { convertPathToAlias } = require("./scripts/vite/internal");
 
 /**
  *
  * @param {string} basePath
  */
-const getDefaultConfig = (basePath = __dirname) =>
-	defineConfig({
-		server: {
-			port: 3000,
-			host: "0.0.0.0",
+const getDefaultConfig = (basePath = __dirname) => ({
+	server: {
+		port: 3000,
+		host: "0.0.0.0",
+	},
+	resolve: {
+		alias: convertPathToAlias(tsconfig.compilerOptions.paths, basePath),
+	},
+	plugins: [
+		react({
+			tsDecorators: true,
+		}),
+		eslint({
+			failOnWarning: false,
+		}),
+	],
+	build: {
+		rollupOptions: {
+			input: {
+				app: "./public/index.html",
+			},
 		},
-		resolve: {
-			alias: convertPathToAlias(tsconfig.compilerOptions.paths, basePath),
-		},
-		plugins: [
-			react({
-				tsDecorators: true,
-			}),
-			eslint({
-				failOnWarning: false,
-			}),
-		],
-	});
+	},
+});
 
 module.exports = {
 	getDefaultConfig,
