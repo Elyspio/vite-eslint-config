4c56cd09d206737a112dc360f34e6ad022d58850
Jonathan Guichard <jona.guich69@gmail.com>
2023-04-23 02:08:02 +0200

    Config -- Eslint reformate config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,89 +1,89 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-  },
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
-  ignorePatterns: ["**/generated/v*.ts", "**/*.scss", "**/*.png", "**/*.svg"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaFeatures: {
-      modules: true,
-      jsx: true,
-    },
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  plugins: ["prettier", "import", "@typescript-eslint", "react", "react-hooks"],
-  rules: {
-    "@typescript-eslint/ban-ts-comment": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/no-empty-interface": "off",
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-non-null-assertion": "off",
-    "@typescript-eslint/no-shadow": [
-      "error",
-      {
-        allow: ["_"],
-      },
-    ],
-    "@typescript-eslint/no-unused-vars": [
-      "error",
-      {
-        argsIgnorePattern: "^_",
-        caughtErrorsIgnorePattern: "^_",
-        varsIgnorePattern: "^_",
-      },
-    ],
-    "@typescript-eslint/no-use-before-define": "off",
-    "import/extensions": "off",
-    "import/no-extraneous-dependencies": "off",
-    "import/order": "off",
-    "import/prefer-default-export": "off",
-    "jsx-a11y/click-events-have-key-events": "off",
-    "jsx-a11y/control-has-associated-label": "off",
-    "jsx-a11y/interactive-supports-focus": "off",
-    "jsx-a11y/no-static-element-interactions": "off",
-    "linebreak-style": "off",
-    "max-classes-per-file": "off",
-    "no-mixed-spaces-and-tabs": "off",
-    "no-shadow": "off",
-    "no-unused-vars": "off",
-    "prettier/prettier": [
-      "error",
-      {
-        bracketSpacing: true,
-        endOfLine: "auto",
-        printWidth: 180,
-        singleQuote: false,
-        tabWidth: 4,
-        useTabs: true,
-      },
-    ],
-    "react-hooks/exhaustive-deps": "error",
-    "react-hooks/rules-of-hooks": "error",
-    "react/jsx-filename-extension": [
-      "warn",
-      {
-        extensions: [".tsx"],
-      },
-    ],
-    "react/jsx-props-no-spreading": "off",
-    "react/no-array-index-key": "off",
-    "react/prop-types": "off",
-  },
-  settings: {
-    "import/resolver": {
-      typescript: {
-        alwaysTryTypes: true,
-      },
-      node: {
-        extensions: [".ts", ".tsx"],
-      },
-    },
-    react: {
-      version: "detect",
-    },
-  },
+	env: {
+		browser: true,
+		es6: true,
+	},
+	extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+	ignorePatterns: ["**/generated/v*.ts", "**/*.scss", "**/*.png", "**/*.svg"],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaFeatures: {
+			modules: true,
+			jsx: true,
+		},
+		ecmaVersion: "latest",
+		sourceType: "module",
+	},
+	plugins: ["prettier", "import", "@typescript-eslint", "react", "react-hooks"],
+	rules: {
+		"@typescript-eslint/ban-ts-comment": "off",
+		"@typescript-eslint/explicit-function-return-type": "off",
+		"@typescript-eslint/explicit-module-boundary-types": "off",
+		"@typescript-eslint/no-empty-interface": "off",
+		"@typescript-eslint/no-explicit-any": "off",
+		"@typescript-eslint/no-non-null-assertion": "off",
+		"@typescript-eslint/no-shadow": [
+			"error",
+			{
+				allow: ["_"],
+			},
+		],
+		"@typescript-eslint/no-unused-vars": [
+			"error",
+			{
+				argsIgnorePattern: "^_",
+				caughtErrorsIgnorePattern: "^_",
+				varsIgnorePattern: "^_",
+			},
+		],
+		"@typescript-eslint/no-use-before-define": "off",
+		"import/extensions": "off",
+		"import/no-extraneous-dependencies": "off",
+		"import/order": "off",
+		"import/prefer-default-export": "off",
+		"jsx-a11y/click-events-have-key-events": "off",
+		"jsx-a11y/control-has-associated-label": "off",
+		"jsx-a11y/interactive-supports-focus": "off",
+		"jsx-a11y/no-static-element-interactions": "off",
+		"linebreak-style": "off",
+		"max-classes-per-file": "off",
+		"no-mixed-spaces-and-tabs": "off",
+		"no-shadow": "off",
+		"no-unused-vars": "off",
+		"prettier/prettier": [
+			"error",
+			{
+				bracketSpacing: true,
+				endOfLine: "auto",
+				printWidth: 180,
+				singleQuote: false,
+				tabWidth: 4,
+				useTabs: true,
+			},
+		],
+		"react-hooks/exhaustive-deps": "error",
+		"react-hooks/rules-of-hooks": "error",
+		"react/jsx-filename-extension": [
+			"warn",
+			{
+				extensions: [".tsx"],
+			},
+		],
+		"react/jsx-props-no-spreading": "off",
+		"react/no-array-index-key": "off",
+		"react/prop-types": "off",
+	},
+	settings: {
+		"import/resolver": {
+			typescript: {
+				alwaysTryTypes: true,
+			},
+			node: {
+				extensions: [".ts", ".tsx"],
+			},
+		},
+		react: {
+			version: "detect",
+		},
+	},
 };
---
+++ .prettierrc.js
@@ -0,0 +1,3 @@
+const config = require("./.eslintrc.js");
+
+module.exports = config.rules["prettier/prettier"][1];
