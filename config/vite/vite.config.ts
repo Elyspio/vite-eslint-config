62164f6d1ee473cf717db8459a8127d633920d72
Jonathan Guichard <jona.guich69@gmail.com>
2023-05-29 22:24:18 +0200

    Config -- Add options to disable chunking and imports alter

--- config/vite/vite.config.ts
+++ config/vite/vite.config.ts
@@ -7,19 +7,22 @@ import { convertPathToAlias } from "./internal.vite";
 import vitePluginImport from "vite-plugin-babel-import";
 import checker from "vite-plugin-checker";
 
-export const getDefaultConfig = (basePath: string = __dirname): UserConfig => ({
-	build: {
-		rollupOptions: {
-			output: {
-				manualChunks(id) {
-					// if (id.includes("node_modules/@mui")) return "mui.vendor";
-					// if (id.includes("node_modules/react/" || "node_modules/react-dom/")) return "react.vendor";
-					if (id.includes("node_modules")) return "vendor";
-				},
-			},
-		},
-	},
-	plugins: [
+
+type GetConfigParams = {
+	basePath?: string,
+	preserveImports?: boolean,
+	disableChunks?: boolean
+}
+
+
+export const getDefaultConfig = ({ basePath, preserveImports, disableChunks }: GetConfigParams): UserConfig => {
+
+	basePath ??= __dirname,
+		preserveImports ??= false;
+	disableChunks ??= false;
+
+
+	const plugins = [
 		svgr(),
 		react({
 			tsDecorators: true,
@@ -29,7 +32,15 @@ export const getDefaultConfig = (basePath: string = __dirname): UserConfig => ({
 			fix: true,
 			cache: false,
 		}),
-		vitePluginImport([
+		checker({
+			// e.g. use TypeScript check
+			typescript: true,
+			
+		}),
+	];
+
+	if (!preserveImports) {
+		plugins.push(vitePluginImport([
 			{
 				ignoreStyles: [],
 				libraryName: "@mui/icons-material",
@@ -42,20 +53,32 @@ export const getDefaultConfig = (basePath: string = __dirname): UserConfig => ({
 				libraryDirectory: "",
 				libraryChangeCase: "pascalCase"
 			},
+		]))
+	}
 
-		]),
-		checker({
-			// e.g. use TypeScript check
-			typescript: true,
-		}),
-	],
-	resolve: {
-		alias: convertPathToAlias(tsconfig.compilerOptions.paths, basePath),
-	},
-	server: {
-		port: 3000,
-		host: "0.0.0.0",
-	},
-});
+	return ({
+		build: {
+			rollupOptions: {
+				output: {
+					manualChunks(id) {
+						if (disableChunks) return undefined;
+						// if (id.includes("node_modules/@mui")) return "mui.vendor";
+						// if (id.includes("node_modules/react/" || "node_modules/react-dom/")) return "react.vendor";
+						if (id.includes("node_modules"))
+							return "vendor";
+					},
+				},
+			},
+		},
+		plugins,
+		resolve: {
+			alias: convertPathToAlias(tsconfig.compilerOptions.paths, basePath),
+		},
+		server: {
+			port: 3000,
+			host: "0.0.0.0",
+		},
+	});
+};
 
 export * from "./internal.vite";
